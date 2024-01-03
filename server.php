dbd2a284d42eefc3b8c5e0e0f0408513a784cb06
hayadebi <76168948+hayadebi@users.noreply.github.com>
2024-01-03 14:02:58 +0900

    a
    a

---
+++ server.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>IPアドレス取得テスト</title>
+</head>
+<body>
+
+<p>
+<?php
+	echo $_SERVER['REMOTE_ADDR'];
+?>
+</p>
+
+</body>
+</html>
